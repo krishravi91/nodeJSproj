3f06a2468ec82d8ccce2b9fc2b111828c359cafa
krishravi91 <krishnamithiladevi@gmail.com>
2022-01-30 09:11:07 +0530

    jwt

--- routes/users.js
+++ routes/users.js
@@ -1,13 +1,65 @@
 import express from "express";
 import {createUser,genPassword,getuserByName} from "../helper.js";
 import bcrypt from 'bcrypt';
+import jwt from "jsonwebtoken";
 const router = express.Router();
 
-  
 router.post("/signup", async (request, response) => {
+  
+  const {username,password} = request.body;    
+  
+  const isUserExist = await getuserByName(username);
+
+  if(isUserExist){
+    response.status(400).send({message:"Invalid Credentials"});
+    return;
+  }
+
+  if(!/^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@!#%&]).{8,}$/g.test(password)){
+    response.status(400).send({message:"Pattern did not match"});
+    return;
+  }
+
+  const hashedPassword = await genPassword(password);
+  const result =  await createUser({
+    username:username,
+    password: hashedPassword
+  });
+response.send(result);
+  
+});
+
+router.post("/signin", async (request, response) => {
+  
+  const {username,password} = request.body;    
+  
+  const isUserExist = await getuserByName(username);
+
+  if(isUserExist){
+    response.status(400).send({message:"Invalid Credentials"});
+    return;
+  }
+
+  if(!/^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@!#%&]).{8,}$/g.test(password)){
+    response.status(400).send({message:"Pattern did not match"});
+    return;
+  }
+
+  const hashedPassword = await genPassword(password);
+  const result =  await createUser({
+    username:username,
+    password: hashedPassword
+  });
+response.send(result);
+  
+});
+
+
+router.post("/login", async (request, response) => {
   
     const {username,password} = request.body;    
     const userfromDB = await getuserByName(username);
+    console.log("userfromDB",userfromDB);
 
 if(!userfromDB)
     {
@@ -20,26 +72,14 @@ const isPasswordMatch = await bcrypt.compare(password,storedPassword);
 console.log(storedPassword);
 
 if(isPasswordMatch){
-
+  const token = jwt.sign({id:userfromDB._id},process.env.SECRET_KEY);
+  response.send({message:"successful login", token: token});
 } else{
   response.status(401).send({message:" Invalid Credentials"});
-      return;
+  return;
 }
-
-response.send(isPasswordMatch);
-    
+   
 });
 
-// function validate(password) {
-//   console.log(password);
-//   if (validator.isStrongPassword(password, {
-//     minLength: 8, minLowercase: 1,
-//     minUppercase: 1, minNumbers: 1, minSymbols: 1
-//   })) {
-//     setErrorMessage('Is Strong Password');
-//   } else {
-//     setErrorMessage('Is Not Strong Password');
-//   }
-// }
   
 export const usersRouter=router;
